5f05d3b5b2bd2831f112cf3571ccf06ed74867b9
Ilya <ilyaniafiodau@Ilyas-MacBook-Pro.local>
2025-03-09 20:20:46 +0300

    changed minsdk to 25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.bmstu_spotlight"
-        minSdk = 24
+        minSdk = 25
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
