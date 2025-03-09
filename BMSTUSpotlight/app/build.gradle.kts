99e92ce8256223e39d05b778f4d71bc59e916af4
Ilya <ilyaniafiodau@Ilyas-MacBook-Pro.local>
2025-03-09 16:50:18 +0300

    changed sdk version to 35

--- BMSTUSpotlight/app/build.gradle.kts
+++ BMSTUSpotlight/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.bmstu_spotlight"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.bmstu_spotlight"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
