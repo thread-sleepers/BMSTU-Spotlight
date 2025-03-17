c2448d0254cadf3ce47eb8eb401a7effb2e5e240
Ilya <ilyaniafiodau@Ilyas-MacBook-Pro.local>
2025-03-17 14:32:24 +0300

    added material3 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.room.common)
     implementation(libs.androidx.room.common.jvm)
+    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
